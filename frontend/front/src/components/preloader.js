824520f1a7323c75f0207dce3396e8be23bfd4c2
Christ Rimmon <rimmon342@gmail.com>
2024-09-05 18:35:15 +0100

    preloader Installed

---
+++ frontend/front/src/components/preloader.js
@@ -0,0 +1,11 @@
+// Preloader.js
+import React from 'react';
+import './preloader.css'; // Import CSS for styling the preloader
+
+const Preloader = () => (
+  <div className="preloader">
+    <div className="spinner"></div>
+  </div>
+);
+
+export default Preloader;
