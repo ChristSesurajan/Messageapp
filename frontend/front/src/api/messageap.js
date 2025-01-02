da0e0d56c4869ec65cb6cad4fb3bddfedab925e1
Christ Rimmon <rimmon342@gmail.com>
2025-01-02 17:38:17 +0000

    useRef implemented for improve speed

--- frontend/front/src/api/messageap.js
+++ frontend/front/src/api/messageap.js
@@ -137,7 +137,7 @@ function Message() {
    
     <div className="message-list">
       <ul>
-        {[messages.current].map((message, index) => (
+        {messages.current.map((message, index) => (
           
            
           <li 
